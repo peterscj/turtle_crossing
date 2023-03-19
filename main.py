ebec5cc21a600c2b7f8362966240649150934ac5
Cole Peterson <cole@epeterson.net>
2023-03-19 07:33:33 -0700

    Updating main to have multiple car lanes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from player import Player
 from scoreboard import Scoreboard
+from car import Car
 import time
 
 screen = Screen()
@@ -13,6 +14,15 @@ screen.tracer(0)
 player_1 = Player()
 scoreboard = Scoreboard()
 
+# Initialize car
+
+# move speed, following dist, len, wid, start y
+car_1 = Car(1, 25, 0.15, 1, 1, -150)
+car_2 = Car(2, 50, 0.05, 4, 1, -60)
+car_3 = Car(5, 100, 0.75, 2, 1, 50)
+car_4 = Car(2, 200, 0.01, 6, 1, 125)
+car_5 = Car(3, 35, 0.1, 2, 1, 200)
+
 screen.listen()
 
 # Key Bindings
@@ -21,8 +31,14 @@ screen.onkey(player_1.up, "Up")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.01)
     screen.update()
+
+    car_1.lane_management()
+    car_2.lane_management()
+    car_3.lane_management()
+    car_4.lane_management()
+    car_5.lane_management()
 
     # Level completion condition
     if player_1.ycor() > 220:

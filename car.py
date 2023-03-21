5a2d5f721f9b21b4a817a4dcd9d8528d91eb327a
Cole Peterson <cole@epeterson.net>
2023-03-20 17:05:04 -0700

    Adding random color for cars and increasing speed of cars for each level

--- car.py
+++ car.py
@@ -1,24 +1,34 @@
 from turtle import Turtle
-import time
+import turtle
+import random
+turtle.colormode(255)
+
+"""
+"""
 
 class Car(Turtle):
-    def __init__(self,movement_dist, following_dist, speed,len,wid,y):
+    def __init__(self,movement_dist, following_dist, speed,len,y):
         super().__init__()
         self.move_x = -1
         self.move_y = 0
         self.cars = list()
         self.speed = speed
         self.len = len
-        self.wid = wid
+        self.wid = 1
         self.x = 270
         self.y = y
+        self.collision = False
         self.movement_dist = movement_dist
         self.following_dist = following_dist
         self.make_car()
 
     def make_car(self):
         new_car = Turtle(shape="square")
-        new_car.color("white")
+        colors = list()
+        for _ in range(3):
+            rand_color = random.randint(1,255)
+            colors.append(rand_color)
+        new_car.color(tuple(colors))
         new_car.shapesize(stretch_len=self.len, stretch_wid=self.wid)
         new_car.penup()
         new_car.goto(self.x,self.y)
@@ -44,7 +54,7 @@ class Car(Turtle):
             starting_pos_y = self.cars[-1].ycor()
             self.cars[-1].goto(starting_pos_x, starting_pos_y)
 
-
-    # def car_speed(self):
-
-
+    def detect_collision(self, player):
+        for car in self.cars:
+            if car.distance(player) < 30:
+                self.collision = True
